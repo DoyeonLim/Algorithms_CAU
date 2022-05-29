69213da7bb40d7273a25f9727dcf8f9fedfb2f04
DoyeonLim <doyeon010420@gmail.com>
2022-05-29 21:43:49 +0900

    modify the number2 code

--- Assignment4/Number2.c
+++ Assignment4/Number2.c
@@ -4,20 +4,15 @@
 
 #pragma warning(disable : 4996)
 
-int main()
+int num[8];
+int matrix[8][11][11];
+int a[11][11];
+int b[11][11];
+
+void make_matrix()
 {
-	int matrix[8][11][11];
-	int m1[11][11];
-	int m2[11][11];
-	int num[8];
-
-	srand(time(NULL));
-
-	int* result;
-	int min;
-
-	printf("Matrix <?,?,?,?> = ");
-	scanf("<%d,%d,%d,%d>", &num[0], &num[1], &num[2], &num[3]);
+	printf("Matrix -> ");
+	scanf("%d %d %d %d", &num[0], &num[1], &num[2], &num[3]);
 
 	for (int i = 1; i < 4; i++)
 	{
@@ -25,80 +20,38 @@ int main()
 		{
 			for (int k = 1; k < num[i] + 1; k++)
 			{
-				matrix[i][j][k] = rand();
+				matrix[i][j][k] = rand() % 100;
 			}
 		}
 	}
 
-	for (int k = 2; k <= 3; k++)
-	{
-		for (int i = 1; i <= 3 - k + 1; i++)
-		{
-			int j = i + k - 1;
-
-			m1[i][j] = 1e5;
-
-			for (int k = i; k < j; k++)
-			{
-				min = m1[i][k] + m1[k + 1][j] + num[i - 1] * num[k] * num[j];
-
-				if (min < m1[i][j])
-				{
-					m1[i][j] = min;
-					m2[i][j] = k;
-				}
-			}
-		}
-	}
-
-	printf("Minimum number of computations = %d\n", m1[1][3]);
-
-	printf("Optimal chain order = ");
-	result = recursion(1, 3);
-	printf("\n");
-
-	printf("Output Matrix\n");
-
-	for (int i = 1; i < num[0] + 1; i++)
-	{
-		for (int j = 1; j < num[3] + 1; j++)
-		{
-			printf("%6d", *(result + i * 11 + j));
-		}
-		printf("\n");
-	}
-
-	return 0;
+	return matrix;
 }
 
-int* matrix_cal(static int* n1, static int* n2, int n3, int mid, int n4) 
+static int* matrix_cal(static int* n1, static int* n2, int n3, int n4, int cen) 
 {
-	int num[8];
-
-	int* result = (int*)malloc(sizeof(int) * 121);
+	int* ans = (int*)malloc(sizeof(int) * 121);
 
 	for (int i = 1; i < num[n3] + 1; i++) 
 	{
 		for (int j = 1; j < num[n4] + 1; j++) 
 		{
-			*(result + i * 11 + j) = 0;
+			*(ans + i * 11 + j) = 0;
 
-			for (int k = 1; k < num[mid] + 1; k++) 
+			for (int k = 1; k < num[cen] + 1; k++) 
 			{
-				*(result + i * 11 + j) = *(result + i * 11 + j) + *(n1 + i * 11 + k) * *(n2 + k * 11 + j);
+				*(ans + i * 11 + j) = *(ans + i * 11 + j) + *(n1 + i * 11 + k) * *(n2 + k * 11 + j);
 			}
 		}
 	}
-	return result;
+
+	return ans;
 }
 
-int* recursion(int first, int finish) 
+static int* re(int first, int finish) 
 {
 	int* n1, * n2;
-	int * result;
-
-	int matrix[8][11][11];
-	int m2[11][11];
+	int * ans;
 
 	if (first == finish) 
 	{
@@ -108,12 +61,70 @@ int* recursion(int first, int finish)
 	}
 
 	printf("(");
-	
-	n1 = recursion(first, m2[first][finish]);
-	n2 = recursion(m2[first][finish] + 1, finish);
-	result = matrix_cal(n1, n2, first - 1, m2[first][finish], finish);
+
+	n1 = re(first, b[first][finish]);
+	n2 = re(b[first][finish] + 1, finish);
+
+	ans = matrix_cal(n1, n2, first - 1, finish, b[first][finish]);
 
 	printf(")");
 
-	return result;
+	return ans;
+}
+
+void output_matrix()
+{
+	int* result;
+
+	printf("\nOutput Matrix\n");
+
+	for (int i = 1; i < num[0] + 1; i++)
+	{
+		for (int j = 1; j < num[3] + 1; j++)
+		{
+			printf("%10d", *(result + i * 11 + j));
+		}
+		printf("\n");
+	}
+}
+
+int main() 
+{
+	srand(time(NULL));
+
+	int min;
+	int* result;
+
+	make_matrix();
+
+	for (int k = 2; k < 4; k++) 
+	{
+		for (int i = 1; i <= 3 - k + 1; i++) 
+		{
+			int j = i + k - 1;
+
+			a[i][j] = 1e5;
+
+			for (int k = i; k < j; k++) 
+			{
+				min = a[i][k] + a[k + 1][j] + num[i - 1] * num[k] * num[j];
+
+				if (min < a[i][j]) 
+				{
+					b[i][j] = k;
+					a[i][j] = min;
+				}
+			}
+		}
+	}
+
+	printf("\nMinimum number of computations = %d\n", a[1][3]);
+	
+	printf("Optimal chain order = ");
+	result = re(1, 3);
+	printf("\n");
+
+	output_matrix();
+
+	return 0;
 }
